fa50dc67f3a1af99a804295bb7b92c3001a0fc3e
Leon Tan <leontan@Leons-MacBook-Pro.local>
2025-01-30 19:39:14 -0700

    start header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  
+    return (
+      <>
+        <div>
+          Leon Yee Leong Tan
+        </div>
+        <div>
+          <div>Bio</div>
+          <div>Career</div>
+          <div>Interests</div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Header
