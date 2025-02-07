e61d2fb5be5143d97f532a7c579d42c714107553
Leon Tan <yltan828@gmail.com>
2025-02-07 15:19:38 -0700

    initialize bullet component

---
+++ src/components/InfoSections/components/Bullets.tsx
@@ -0,0 +1,23 @@
+import '../../../styles/info_sections/components/Bullets.css';
+
+type BulletsProp = {
+  title: string;
+  body: bodyJSON[];
+};
+
+type bodyJSON = {
+  bulletPoint: string;
+  desc: string;
+};
+
+function Bullets({title, body}: BulletsProp) {
+  
+  return (
+    <div className='bullets'>
+      <div className='title'>{title}</div>
+      <div className='body'>{}</div>
+    </div>
+  );
+};
+  
+export default Bullets;
