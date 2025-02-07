19def587cb31e55749fa779be58075162e3f0322
Leon Tan <yltan828@gmail.com>
2025-02-07 15:58:34 -0700

    make and display BulletPoint component

--- src/components/InfoSections/components/Bullets.tsx
+++ src/components/InfoSections/components/Bullets.tsx
@@ -15,9 +15,27 @@ function Bullets({title, body}: BulletsProp) {
   return (
     <div className='bullets'>
       <div className='title'>{title}</div>
-      <div className='body'>{}</div>
+      <div className='body'>
+        {body.map((bulletEntry) => {
+          const { bulletPoint, desc } = bulletEntry;
+
+          return <BulletPoint
+                  bulletPoint={bulletPoint}
+                  desc={desc}
+                />;
+        })}
+      </div>
     </div>
   );
 };
+
+function BulletPoint({bulletPoint, desc}: bodyJSON) {
+  return (
+    <>
+      <div>&#x2022; {bulletPoint}</div>
+      <div>{desc}</div>
+    </>
+  );
+}
   
 export default Bullets;
