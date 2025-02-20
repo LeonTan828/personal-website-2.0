77e2f47f3c0c76d20b8d5ca6c7adc7a5e795e18b
Leon Tan <yltan828@gmail.com>
2025-02-20 16:47:54 -0700

    link footer buttons

--- src/components/Footer/Button.tsx
+++ src/components/Footer/Button.tsx
@@ -2,14 +2,18 @@ import '../../styles/Header.css';
 
 type ButtonProps = {
   title: string;
+  link: string;
+  openOnNewTab?: boolean;
 };
 
-function Button({title}: ButtonProps) {
+function Button({title, link, openOnNewTab}: ButtonProps) {
   
+  const target = openOnNewTab ? '_target' : '';
+
   return (
-    <div className='footer-button clickable'>
+    <a className='footer-button clickable' href={link} target={target}>
       {title}
-    </div>
+    </a>
   );
 };
   
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import Button from './Button';
 
+import resume from '../../assets/documents/Leon\'s Resume.pdf';
+
 import '../../styles/Footer.css';
 
 function Footer() {
@@ -8,15 +10,23 @@ function Footer() {
     <div className='footer'>
       <Button
         title='Email'
+        link='mailto:leontan.se@gmail.com'
+        openOnNewTab
       />
       <Button
         title='LinkedIn'
+        link='https://www.linkedin.com/in/leon-yl-tan'
+        openOnNewTab
       />
       <Button
         title='Github'
+        link='https://github.com/LeonTan828'
+        openOnNewTab
       />
       <Button
         title='Resume'
+        link={resume}
+        openOnNewTab
       />
     </div>
   );
