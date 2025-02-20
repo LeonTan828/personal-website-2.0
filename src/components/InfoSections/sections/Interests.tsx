bb31943b8e0d5f3998ae5fbbbbebadfd182182b6
Leon Tan <yltan828@gmail.com>
2025-02-20 00:13:42 -0700

    WIP more styling adjustments

--- src/components/InfoSections/sections/Interests.tsx
+++ src/components/InfoSections/sections/Interests.tsx
@@ -2,13 +2,13 @@ import Astrophotography from "./interest-sections/Astrophotography";
 import Hiking from "./interest-sections/Hiking";
 import Movies from "./interest-sections/Movies";
 
-// import '../../../styles/info_sections/Interests.css';
+import '../../../styles/info_sections/Interests.css';
 
 function Interests() {
   
   return (
     <div className='interests'>
-      <div className='title'>Interests</div>
+      <div className='interest-title'>Interests</div>
       <Astrophotography />
       <Movies />
       <Hiking />
