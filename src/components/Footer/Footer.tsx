87bacc54b45b778ea92cdfe3bf04dbb5a34bb068
Leon Tan <yltan828@gmail.com>
2025-02-20 17:00:26 -0700

    adjust styling for footer and add page top link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,26 +8,34 @@ function Footer() {
   
   return (
     <div className='footer'>
-      <Button
-        title='Email'
-        link='mailto:leontan.se@gmail.com'
-        openOnNewTab
-      />
-      <Button
-        title='LinkedIn'
-        link='https://www.linkedin.com/in/leon-yl-tan'
-        openOnNewTab
-      />
-      <Button
-        title='Github'
-        link='https://github.com/LeonTan828'
-        openOnNewTab
-      />
-      <Button
-        title='Resume'
-        link={resume}
-        openOnNewTab
-      />
+      <div className='left-footer'>
+        <Button
+          title='Leon Yee Leong Tan'
+          link='#page-top'
+        />
+      </div>
+      <div className='right-footer'>
+        <Button
+          title='Email'
+          link='mailto:leontan.se@gmail.com'
+          openOnNewTab
+        />
+        <Button
+          title='LinkedIn'
+          link='https://www.linkedin.com/in/leon-yl-tan'
+          openOnNewTab
+        />
+        <Button
+          title='Github'
+          link='https://github.com/LeonTan828'
+          openOnNewTab
+        />
+        <Button
+          title='Resume'
+          link={resume}
+          openOnNewTab
+        />
+      </div>
     </div>
   );
 };
