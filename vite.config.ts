0df74092c3c2e9019a06a9b0f3e73d8ae99beb41
Leon Tan <yltan828@gmail.com>
2025-02-21 17:14:08 -0700

    debug deployment again

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal-website-2.0/",
+  base: "./personal-website-2.0",
   plugins: [react()],
 })
